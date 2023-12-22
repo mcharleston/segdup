05c33c7d435d0caaa95ffa954ecf51b4b4346561
mcharleston <michael.charleston@utas.edu.au>
2023-12-22 13:34:16 +1100

    Fixed formatting for consistency

--- src/EmptyMove.cpp
+++ src/EmptyMove.cpp
@@ -38,7 +38,15 @@ void EmptyMove::apply(CophyMultiMap& CMM, double T) {
 
 	Tree* s = CMM.getHostTree();
 	inversenodemap& invMap = CMM.getInverseMap();
-	DEBUG(for(auto v : s->getVertices()) { cout << v.second->getLabel() << ": "; for (auto n : invMap[v.second]) cout << n->getLabel() << " "; cout << endl; })
+	DEBUG(
+		for(auto v : s->getVertices()) {
+			cout << v.second->getLabel() << ": ";
+			for (auto n : invMap[v.second]) {
+				cout << n->getLabel() << " ";
+			}
+			cout << endl;
+		}
+	)
 	vector<Node*> fromVertices;
 	for (auto v : s->getVertices()) {
 		int dupsAtV = 0;
@@ -49,7 +57,13 @@ void EmptyMove::apply(CophyMultiMap& CMM, double T) {
 		if (dupsAtV > 1)
 			fromVertices.push_back(v.second);
 	}
-	DEBUG(cout << "fromVertices: "; for (auto v : fromVertices) cout << v->getLabel() << " "; cout << endl;)
+	DEBUG(
+		cout << "fromVertices: ";
+		for (auto v : fromVertices) {
+			cout << v->getLabel() << " ";
+		}
+		cout << endl;
+	)
 
 	if (fromVertices.size() == 0)
 		return;
@@ -126,7 +140,13 @@ void EmptyMove::apply(CophyMultiMap& CMM, double T) {
 		if (v == top)
 			break;
 	}
-	DEBUG(cout << "toVertices: "; for (auto v : toVertices) cout << v.first->getLabel() << " "; cout << endl;)
+	DEBUG(
+		cout << "toVertices: ";
+		for (auto v : toVertices) {
+			cout << v.first->getLabel() << " ";
+		}
+		cout << endl;
+	)
 	DEBUG(cout << "total is " << total << endl;)
 	
 	//resample vertex
