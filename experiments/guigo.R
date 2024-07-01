dcfceefd48d6a6777717ab5742de3a59d26a273f
Yao-ban Chan <yaoban@unimelb.edu.au>
2024-07-01 12:10:24 +1000

    more guigo

--- experiments/guigo.R
+++ experiments/guigo.R
@@ -3,4 +3,6 @@ gdat <- read.csv("guigo_temps.csv",header=T)
 plot(s7~t,data=gdat)
 points(s10~t,data=gdat,col="red")
 
-with(gdat,plot(s7/(s7+s10)))
+pdf("guigo_temps.pdf")
+with(gdat,plot(s7/(s7+s10) ~ t, xlab="Final temperature", ylab="Proportion of s7 duplications"))
+dev.off()
